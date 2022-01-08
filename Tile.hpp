90cb508335f4002639bc6f754140f508e8dca228
Dayshadow <42987997+Dayshadow@users.noreply.github.com>
2022-01-08 00:38:12 -0500

    Delete these inline commands because I didn't understand what they meant

--- Tile.hpp
+++ Tile.hpp
@@ -9,8 +9,8 @@
 class Tile
 {
 public:
-	inline Tile() : chunkPos(glm::ivec2(0, 0)), tileID(0) {};
-	inline Tile(glm::ivec2 p_chunkPos, int p_tileID) : chunkPos(p_chunkPos), tileID(p_tileID) {};
+	Tile() : chunkPos(glm::ivec2(0, 0)), tileID(0) {};
+	Tile(glm::ivec2 p_chunkPos, int p_tileID) : chunkPos(p_chunkPos), tileID(p_tileID) {};
 	glm::ivec2 getChunkPos();
 	int tileID;
 private:
